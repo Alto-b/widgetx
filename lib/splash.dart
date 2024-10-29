cd805c41b93d5498390de5835e9958b6f45faec2
Alto b <altob282@gmail.com>
2024-10-29 15:39:42 +0530

    splash bg updated

--- lib/splash.dart
+++ lib/splash.dart
@@ -4,41 +4,95 @@ import 'package:gap/gap.dart';
 import 'package:get/get.dart';
 import 'package:widgetx/homepage.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
 
   @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    proceedToHome();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Spacer(),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Widget",
-                    style: TextStyle(
-                        color: Colors.grey,
-                        fontWeight: FontWeight.w400,
-                        fontSize: 20),
-                  ),
-                  Text(
-                    "X",
-                    style: TextStyle(
-                        color: Colors.blue,
-                        fontSize: 55,
-                        fontWeight: FontWeight.w700),
-                  ),
-                ],
-              ),
-              Spacer(),
-              GradientActivityIndicator(),
-              Gap(35)
-            ]),
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.blue, Colors.grey, Colors.grey.shade400],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Center(
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Spacer(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "widget",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 20,
+                        letterSpacing: 2,
+                        shadows: [
+                          Shadow(
+                            color: Colors.black.withOpacity(0.3),
+                            blurRadius: 4,
+                            offset: Offset(2, 2),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Text(
+                      "X",
+                      style: TextStyle(
+                        color: Colors.blue[700],
+                        fontSize: 65,
+                        fontWeight: FontWeight.w800,
+                        letterSpacing: 1.5,
+                        shadows: [
+                          Shadow(
+                            color: Colors.black.withOpacity(0.3),
+                            blurRadius: 4,
+                            offset: Offset(2, 2),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Text(
+                      "flutter",
+                      style: TextStyle(
+                        color: Colors.white60,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 20,
+                        letterSpacing: 2,
+                        shadows: [
+                          Shadow(
+                            color: Colors.black.withOpacity(0.3),
+                            blurRadius: 4,
+                            offset: Offset(2, 2),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Spacer(),
+                GradientActivityIndicator(),
+                Gap(55)
+              ]),
+        ),
       ),
     );
   }
@@ -55,14 +109,16 @@ class GradientActivityIndicator extends StatelessWidget {
       child: ShaderMask(
         shaderCallback: (Rect bounds) {
           return const SweepGradient(
-            colors: [
-              Colors.blue,
-              Colors.yellow,
-              Colors.purple,
-              Colors.red,
-              Colors.blue
-            ],
-            stops: [0.2, 0.5, 0.5, 0.5, 1.0],
+            colors: [Colors.white],
+            // colors: [
+            //   Colors.blue,
+            //   Colors.yellow,
+            //   Colors.purple,
+            //   Colors.red,
+            //   Colors.blue
+            // ],
+            stops: [1.0],
+            // stops: [0.2, 0.5, 0.5, 0.5, 1.0],
             startAngle: 0.0,
             endAngle: 1.0,
             tileMode: TileMode.repeated,
@@ -70,7 +126,7 @@ class GradientActivityIndicator extends StatelessWidget {
         },
         child: const CupertinoActivityIndicator(
           color: Colors.white,
-          radius: 16,
+          radius: 25,
         ),
       ),
     );
@@ -79,6 +135,7 @@ class GradientActivityIndicator extends StatelessWidget {
 
 Future<void> proceedToHome() async {
   Future.delayed(const Duration(seconds: 2)).then((value) {
-    Get.offAll(const HomePageView());
+    Get.offAll(const HomePageView(),
+        transition: Transition.downToUp, duration: Duration(seconds: 1));
   });
 }
